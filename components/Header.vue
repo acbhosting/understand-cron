57d25603b8a7ab5925f08df31dc48e5248f5065c
Cătălin Bratu <dev@catalinbratu.com>
2023-05-28 12:22:47 +0300

    feat: add header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="py-4">
+    <UContainer class="flex items-center justify-between">
+      <div>
+        <NuxtLink to="/">
+          Logo
+        </NuxtLink>
+      </div>
+      <div class="flex gap-8">
+        <NuxtLink to="/">
+          Search
+        </NuxtLink>
+        <NuxtLink to="/">
+          Random
+        </NuxtLink>
+        <NuxtLink to="/">
+          Help
+        </NuxtLink>
+      </div>
+    </UContainer>
+  </header>
+</template>
+<script setup lang="ts">
+</script>
